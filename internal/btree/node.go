0e1b532d5f2f72bce77e70691d12a695ee00f725
00Yashwanth00 <rameshyashwanth36@gmail.com>
2026-09-13 09:59:15 +0530

    Node update functions done

--- internal/btree/node.go
+++ internal/btree/node.go
@@ -174,3 +174,142 @@ func nodeLookupLE(node BNode, key []byte) uint16 {
 
 	return found
 }
+
+// --------------------
+// Node update functions
+// --------------------
+
+func nodeAppendKV(
+	new BNode,
+	idx uint16,
+	ptr uint64,
+	key []byte,
+	val []byte,
+) {
+	new.setPtr(idx, ptr)
+
+	pos := new.kvPos(idx)
+
+	binary.LittleEndian.PutUint16(
+		new[pos+0:],
+		uint16(len(key)),
+	)
+
+	binary.LittleEndian.PutUint16(
+		new[pos+2:],
+		uint16(len(val)),
+	)
+
+	copy(new[pos+4:], key)
+
+	copy(
+		new[pos+4+uint16(len(key)):],
+		val,
+	)
+
+	new.setOffset(
+		idx+1,
+		new.getOffset(idx)+
+			4+
+			uint16(len(key)+len(val)),
+	)
+}
+
+func nodeAppendRange(
+	new BNode,
+	old BNode,
+	dstNew uint16,
+	srcOld uint16,
+	n uint16,
+) {
+	for i := uint16(0); i < n; i++ {
+		idxOld := srcOld + i
+		idxNew := dstNew + i
+
+		nodeAppendKV(
+			new,
+			idxNew,
+			old.getPtr(idxOld),
+			old.getKey(idxOld),
+			old.getVal(idxOld),
+		)
+	}
+}
+
+func leafInsert(
+	new BNode,
+	old BNode,
+	idx uint16,
+	key []byte,
+	val []byte,
+) {
+	new.setHeader(
+		BNODE_LEAF,
+		old.nkeys()+1,
+	)
+
+	nodeAppendRange(
+		new,
+		old,
+		0,
+		0,
+		idx,
+	)
+
+	nodeAppendKV(
+		new,
+		idx,
+		0,
+		key,
+		val,
+	)
+
+	nodeAppendRange(
+		new,
+		old,
+		idx+1,
+		idx,
+		old.nkeys()-idx,
+	)
+}
+
+func nodeReplaceKidN(
+	tree *BTree,
+	new BNode,
+	old BNode,
+	idx uint16,
+	kids ...BNode,
+) {
+	inc := uint16(len(kids))
+
+	new.setHeader(
+		BNODE_NODE,
+		old.nkeys()+inc-1,
+	)
+
+	nodeAppendRange(
+		new,
+		old,
+		0,
+		0,
+		idx,
+	)
+
+	for i, node := range kids {
+		nodeAppendKV(
+			new,
+			idx+uint16(i),
+			tree.new(node),
+			node.getKey(0),
+			nil,
+		)
+	}
+
+	nodeAppendRange(
+		new,
+		old,
+		idx+inc,
+		idx+1,
+		old.nkeys()-(idx+1),
+	)
+}
