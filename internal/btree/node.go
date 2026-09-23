459d0dc17dace246eb3ade85f374e63ac42c6ae3
00Yashwanth00 <rameshyashwanth36@gmail.com>
2026-09-23 19:35:49 +0530

    BTree merging logic is done

--- internal/btree/node.go
+++ internal/btree/node.go
@@ -483,3 +483,107 @@ func nodeInsert(
 		split[:nsplit]...,
 	)
 }
+
+func leafDelete(new BNode, old BNode, idx uint16) {
+	new.setHeader(BNODE_LEAF, old.nkeys()-1)
+
+	nodeAppendRange(new, old, 0, 0, idx)
+
+	nodeAppendRange(
+		new,
+		old,
+		idx,
+		idx+1,
+		old.nkeys()-(idx+1),
+	)
+}
+
+func nodeMerge(new BNode, left BNode, right BNode) {
+	new.setHeader(left.btype(), left.nkeys()+right.nkeys())
+
+	nodeAppendRange(
+		new,
+		left,
+		0,
+		0,
+		left.nkeys(),
+	)
+
+	nodeAppendRange(
+		new,
+		right,
+		left.nkeys(),
+		0,
+		right.nkeys(),
+	)
+}
+
+func nodeReplace2Kid(
+	new BNode,
+	old BNode,
+	idx uint16,
+	ptr uint64,
+	key []byte,
+) {
+	new.setHeader(BNODE_NODE, old.nkeys()-1)
+
+	nodeAppendRange(
+		new,
+		old,
+		0,
+		0,
+		idx,
+	)
+
+	nodeAppendKV(
+		new,
+		idx,
+		ptr,
+		key,
+		nil,
+	)
+
+	nodeAppendRange(
+		new,
+		old,
+		idx+1,
+		idx+2,
+		old.nkeys()-(idx+2),
+	)
+}
+
+func shouldMerge(
+	tree *BTree,
+	node BNode,
+	idx uint16,
+	updated BNode,
+) (int, BNode) {
+
+	if updated.nbytes() > BTREE_PAGE_SIZE/4 {
+		return 0, BNode{}
+	}
+
+	// Try the left sibling.
+	if idx > 0 {
+		sibling := BNode(tree.get(node.getPtr(idx - 1)))
+
+		merged := sibling.nbytes() + updated.nbytes() - HEADER
+
+		if merged <= BTREE_PAGE_SIZE {
+			return -1, sibling
+		}
+	}
+
+	// Try the right sibling.
+	if idx+1 < node.nkeys() {
+		sibling := BNode(tree.get(node.getPtr(idx + 1)))
+
+		merged := sibling.nbytes() + updated.nbytes() - HEADER
+
+		if merged <= BTREE_PAGE_SIZE {
+			return +1, sibling
+		}
+	}
+
+	return 0, BNode{}
+}
