4d246f8bf36b7c23a92a66de7460f18bf423b814
00Yashwanth00 <rameshyashwanth36@gmail.com>
2026-09-23 19:18:51 +0530

    BTree insertion interface is implemented

--- internal/btree/tree.go
+++ internal/btree/tree.go
@@ -9,3 +9,37 @@ type BTree struct {
 	new func([]byte) uint64
 	del func(uint64)
 }
+
+func (tree *BTree) Insert(key []byte, val []byte) {
+	if tree.root == 0 {
+		root := BNode(make([]byte, BTREE_PAGE_SIZE))
+		root.setHeader(BNODE_LEAF, 2)
+
+		nodeAppendKV(root, 0, 0, nil, nil)
+
+		nodeAppendKV(root, 1, 0, key, val)
+
+		tree.root = tree.new(root)
+		return
+	}
+
+	node := treeInsert(tree, tree.get(tree.root), key, val)
+
+	nsplit, split := nodeSplit3(node)
+
+	tree.del(tree.root)
+
+	if nsplit > 1 {
+		root := BNode(make([]byte, BTREE_PAGE_SIZE))
+		root.setHeader(BNODE_NODE, nsplit)
+
+		for i, knode := range split[:nsplit] {
+			ptr, key := tree.new(knode), knode.getKey(0)
+			nodeAppendKV(root, uint16(i), ptr, key, nil)
+		}
+
+		tree.root = tree.new(root)
+	} else {
+		tree.root = tree.new(split[0])
+	}
+}
